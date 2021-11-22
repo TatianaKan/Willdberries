af86a04891504042a550361bb946cfe3a25f27ba
TatianaKan <klanukt@gmail.com>
2021-11-22 14:06:18 +0300

    фильтр и поиск

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,32 @@
-const cart = function() {
+const cart = function () {
   const cartBtn = document.querySelector('.button-cart');
-const cart = document.getElementById('modal-cart');
+  const cart = document.getElementById('modal-cart');
 
-const closeBtn = cart.querySelector('.modal-close');
+  const closeBtn = cart.querySelector('.modal-close');
 
-cartBtn.addEventListener('click', function() {
-  cart.style.display = 'flex'
-})
 
-closeBtn.addEventListener('click', function() {
-  cart.style.display = ''
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  cartBtn.addEventListener('click', function () {
+    cart.style.display = 'flex'
+  })
+
+  closeBtn.addEventListener('click', function () {
+    cart.style.display = ''
+  })
 }
 cart();
