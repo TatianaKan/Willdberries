d37437010d2d239398a738def67b44466002ba5c
TatianaKan <klanukt@gmail.com>
2021-11-22 12:05:45 +0300

    исправлен обработчик поиска

--- js/search.js
+++ js/search.js
@@ -1,10 +1,6 @@
 const search = function() {
 const input = document.querySelector('.search-block > input');
-const searchBtn = document.querySelector('.btn-outline-secondary');
-
-input.addEventListener ('input', (e)=> {
-console.log( e.target.value);
-})
+const searchBtn = document.querySelector('.search-block > button');
 
 searchBtn.addEventListener('click', ()=> {
   console.log( input.value);
